7f5ab5fb67b413fe5400b7f31f3b549cb33c4b4e
ecolovecat <tranphuongduy0403@gmail.com>
2024-01-14 18:59:23 +0700

    Add recommended library

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
 
 const app = express();
+
+// Middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "hehe" });
+});
 
 module.exports = app;
